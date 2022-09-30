76e2ffc05d128acb41fe2e7e9c48787a9529bdb8
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-09-30 11:44:25 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_091437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "fullname"
+    t.integer "batch_number"
+    t.text "biography"
+    t.text "contact_info"
+    t.string "company"
+    t.string "location"
+    t.text "skills"
+    t.text "hobby"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
