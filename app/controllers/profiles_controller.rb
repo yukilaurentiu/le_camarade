fc545ee59ce93e776c673c01c06012b6d97e7d58
Sydel Chayat <mayoindonela@yahoo.com>
2022-10-05 00:38:11 +0300

    user profile update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,20 +3,6 @@ class ProfilesController < ApplicationController
     @profiles = Profile.all
   end
 
-  # def create_profile
-  #   Profile.create(user: self)
-  # end
-
-  # def create
-  #   @profile = Profile.new(profile_params)
-  #   @profile.user = current_user
-  #   if profilet.save
-  #     # redirect_to index_path_url
-  #     redirect_to :controller => 'profiles', :action => 'index'
-  #   else
-  #     render :new
-  #   end
-
   def show
     @profiles = Profile.find(params[:id])
   end
@@ -29,10 +15,4 @@ class ProfilesController < ApplicationController
     @profiles = Profile.find(params[:id])
     @profiles.update(params[:profile])
   end
-
-  # private
-
-  # def profile_params
-  #   params.require(:profile).permit(:batch_number, :fullname, :biography, :contact_information, :company, :location, :skills, :hobby)
-  # end
 end
