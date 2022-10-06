45dcf3c5f68b418b9106e74032951f8e5b6f3d7d
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-10-06 15:27:12 +0200

    testing

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -10,6 +10,7 @@ class ChatroomsController < ApplicationController
   def create
     @chatroom = Chatroom.new
     @chatroom.user = current_user
+    @chatroom.receiver = current_user
     if @chatroom.save
       redirect_to chatrooms_path, status: :see_other, notice: "You successfully created a chat"
     end
