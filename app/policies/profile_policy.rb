3c8186ba6145c7f38763604ca3e11b8967885ec7
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-10-21 16:54:02 -0700

    pundit for profile

---
+++ app/policies/profile_policy.rb
@@ -0,0 +1,16 @@
+class ProfilePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+end
