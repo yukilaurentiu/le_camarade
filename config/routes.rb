2d443f720ba9a195e37ade2065c652fe10a49599
oliver stoislow <oliver.stoislow@gmx.de>
2022-10-24 16:17:02 +0200

    I solved an error which came up when signing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :profiles
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controller: { sessions: 'users/sessions' }
   root to: "profiles#index"
   # root to: "users#index"
 
